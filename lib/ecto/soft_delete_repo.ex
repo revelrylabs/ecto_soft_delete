cbc73187a1855aa77a262af34a3be9f2be18bf09
Thiago Santos <thia.mdossantos@gmail.com>
2019-02-25 22:09:32 -0300

    Add soft_delete/1

--- lib/ecto/soft_delete_repo.ex
+++ lib/ecto/soft_delete_repo.ex
@@ -1,10 +1,21 @@
 defmodule Ecto.SoftDelete.Repo do
   @callback soft_delete_all(queryable :: Ecto.Queryable.t()) :: {integer, nil | [term]}
+  @callback soft_delete(struct_or_changeset :: Ecto.Schema.t() | Ecto.Changeset.t()) :: {:ok, Ecto.Schema.t()} | {:error, Ecto.Changeset.t()}
 
   defmacro __using__(_opts) do
     quote do
       def soft_delete_all(queryable) do
-        update_all(queryable, set: [deleted_at: DateTime.utc_now()])
+        update_all(queryable, set: [deleted_at: utc_now()])
+      end
+
+      def soft_delete(struct_or_changeset) do
+        struct_or_changeset
+        |> Ecto.Changeset.change(deleted_at: utc_now())
+        |> update()
+      end
+
+      defp utc_now do
+        DateTime.truncate(DateTime.utc_now(), :second)
       end
     end
   end
--- test/soft_delete_repo_test.exs
+++ test/soft_delete_repo_test.exs
@@ -17,7 +17,26 @@ defmodule Ecto.SoftDelete.Repo.Test do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Ecto.SoftDelete.Test.Repo)
   end
 
-  describe "soft_delete_all" do
+  describe "soft_delete/1" do
+    test "should soft delete the queryable" do
+      user = Repo.insert!(%User{email: "test0@example.com"})
+
+      assert {:ok, %User{}} = Repo.soft_delete(user)
+
+      assert Repo.get_by!(User, email: "test0@example.com").deleted_at != nil
+    end
+
+    test "should return an error deleting" do
+      user = Repo.insert!(%User{email: "test0@example.com"})
+      Repo.delete!(user)
+
+      assert_raise Ecto.StaleEntryError, fn ->
+        Repo.soft_delete(user)
+      end
+    end
+  end
+
+  describe "soft_delete_all/1" do
     test "soft deleted the query" do
       Repo.insert!(%User{email: "test0@example.com"})
       Repo.insert!(%User{email: "test1@example.com"})
