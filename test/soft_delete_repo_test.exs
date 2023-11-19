40952c537d2ba4a01f70a26529bfa18aea5e3d94
mithereal <mithereal@gmail.com>
2023-11-19 02:19:27 -0700

    fix travis test

--- test/soft_delete_repo_test.exs
+++ test/soft_delete_repo_test.exs
@@ -92,7 +92,7 @@ defmodule Ecto.SoftDelete.Repo.Test do
 
   describe "soft_restore/1" do
     test "should soft restore the queryable" do
-      Repo.insert!(%User{email: "test0@example.com"}) |> Repo.soft_restore()
+      Repo.insert!(%User{email: "test0@example.com"}) |> Repo.soft_restore(Repo)
 
       assert Repo.get_by!(User, [email: "test0@example.com"], with_deleted: true).deleted_at ==
                nil
